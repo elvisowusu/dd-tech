bb1cd1347f6652703980ac376132dc58ac776675
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-06-30 15:46:36 +0000

    feat: Clean up Home component by removing unnecessary code and improving structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,30 +9,6 @@ import WhyChooseSection from "./components/whyChooseUs";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.okay
-          </li>
-        </ol>
-=======
     <>
     <HeroSection/>
     <AboutUs/>
@@ -40,7 +16,6 @@ export default function Home() {
     <Services/>
     <WhyChooseSection/>
     <PartnerSection/>
->>>>>>> e45ae3a449e09fce6dffd73290a915f88b73e406
 
     </>
   );
